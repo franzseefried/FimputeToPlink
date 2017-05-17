411c4e4b13a7fbec7c6e77c0c430d9d733995d82
franzseefried <franz.seefried@qualitasag.ch>
2017-05-17 13:08:39 +0200

    completed C++ function while loop

--- src/rplcString.cpp
+++ src/rplcString.cpp
@@ -1,5 +1,6 @@
 #include "rplcString.h"
 #include <string>
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
@@ -13,13 +14,47 @@ replaceString::~replaceString()
 
 std::string replaceString::recodeGenotypes(std::string s)
 {
+  //Loop over all possible genotype calls
+  int gtc=0;
+  while (gtc < 10)
+  {
+    string replacedString;
+        
+        if      (gtc==0){
+          replacedString=("A A ");
+        }
+        else if (gtc==1){
+          replacedString=("A B ");
+        }
+        else if (gtc==2){
+          replacedString=("B B ");
+        }
+        else            {
+          replacedString=("Z Z ");
+        }
+         
+        
+        //convert int to string
+        std::string sti = std::to_string(gtc);
+        string stringToReplace(sti);
+        
+        int pos = s.find(stringToReplace);
+        //cout <<pos<<" "<<string::npos<<endl; 
+        
+        while(pos != string::npos)
+        {
+          //Erase the targeted string at the location we set
+          s.erase(pos,stringToReplace.length());
+          //Insert the new string where we last deleted the old string
+          s.insert(pos,replacedString);
+          //Get position of targeted string to erase
+          pos = s.find(stringToReplace);
+        }
+         
+        ++gtc;
+  }
 
-  return s;
-  /* 
-    std::string sout=s;
-    sout=s.replace(s.begin(),s.end(),'3','1');
-    return sout;
-  */
+   return s;
 
 }
 
